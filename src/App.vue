973ff6d128f1cac0adbeb6e02901b122540abb06
BRIVAL-M <brivalmickael@gmail.com>
2022-05-20 11:29:59 +0200

    Signin and news page

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+
+
+
+
+ <script>
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "../App.css";
+import Signup from "./components/Signup.vue";
+//import Nav from "./components/Navbar.vue";
+
+
+
+
+export default {
+  name: "App",
+  components: {
+    Signup,
+ 
+  
+}
+};
+
+
+
+</script>
+<template>
+
+ 
+  
+
+
+
+<router-view></router-view> 
+
+</template>
+
+<style>
+
+
+</style>
